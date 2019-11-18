606faffb9c509a64a71d5065042e7a19cdc248d7
Anil Pavan <appkumark@yahoo.com>
2019-11-19 02:21:30 +0530

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    console.log("Constructor");
+    super(props);
+
+    this.state = {
+      counter: 0,
+      seed: 0
+    };
+
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed
+      };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("Component Did Mount");
+    console.log("==================");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp
+    ) {
+      console.log("Should Component Update - DO NOT RENDER");
+      console.log("==================");
+      return false;
+    }
+    console.log("Should Component Update - RENDER");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("Get Snapshot Before update");
+    return null;
+  }
+
+  render() {
+    console.log("Render");
+
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <div className="counter">Counter: {this.state.counter}</div>
+      </div>
+    );
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Component Did Update");
+    console.log("==================");
+  }
+  componentWillUnmount() {
+    console.log("Component Will Unmount");
+    console.log("==================");
+  }
+
+  componentDidCatch(error, info) {
+    console.log("Component Did Catch");
+  }
+}
+
+export default Counter;
